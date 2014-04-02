6f20a52b3a8f0ec482e5f352492c0e833b388af5
cli9 <cli9@ncsu.edu>
2014-04-02 05:48:36 -0400

    Modification

--- demo_matriximpute.m
+++ demo_matriximpute.m
@@ -7,9 +7,9 @@ RandStream.setGlobalStream(s);
 
 p1 = 500;
 p2 = 500;
-r = 3;
+r = 5;
 
-M = randn(p1,r) * randn(r,p2) + randn(p1,p2);
+M = randn(p1,r) * randn(r,p2); %+ randn(p1,p2);
 display(rank(M));
 
 missingprop = 0.75;
@@ -26,12 +26,13 @@ maxlambda = stats.maxlambda;
 disp(maxlambda);
 
 %%
-gridpts = 11;
+gridpts = 5;
 lambdas = exp(log(maxlambda)/gridpts*(gridpts:-1:1));
 
+display('structure_svt');
 % solution path by warm start
 tic;
-profile on;
+%profile on;
 for i=1:gridpts
     if (i==1)
         Y0 = [];
@@ -39,13 +40,111 @@ for i=1:gridpts
         Y0 = Z;
     end
     %[Z,stats] = matrix_impute_Nesterov(Mobs,lambdas(i),'Y0',Y0,'Display','off');
-    [Z,stats] = matrix_impute_MM(Mobs,lambdas(i),'Y0',Y0,'Display','off');
+    [Z,stats] = MatrixCompletion_MM(Mobs,lambdas(i),'Y0',Y0,'Display','off');
     
     if i >= 3
         display(['Grid point ' num2str(i) ', rank=' num2str(stats.rank)]);
     end
 end
-profile viewer;
+%profile viewer;
 toc;
 
+%% generate a random matrix with missing entries
+clear;
 
+s = RandStream('mt19937ar','Seed',2014);  % Reset random seed
+RandStream.setGlobalStream(s);
+
+p1 = 500;
+p2 = 500;
+r = 5;
+
+M = randn(p1,r) * randn(r,p2); %+ randn(p1,p2);
+display(rank(M));
+
+missingprop = 0.75;
+missingidx = rand(p1,p2)<missingprop;
+
+Mobs = M;
+Mobs(missingidx) = nan;
+
+%% find max lambda for nuclear norm regularization
+
+%[~,stats] = matrix_impute_Nesterov(Mobs,inf);
+[~,stats] = matrix_impute_MM(Mobs,inf);
+maxlambda = stats.maxlambda;
+disp(maxlambda);
+
+%%
+gridpts = 5;
+lambdas = exp(log(maxlambda)/gridpts*(gridpts:-1:1));
+
+display('svt');
+tic;
+%profile on;
+for i=1:gridpts
+    if (i==1)
+        Y0 = [];
+    else
+        Y0 = Z;
+    end
+    %[Z,stats] = matrix_impute_Nesterov(Mobs,lambdas(i),'Y0',Y0,'Display','off');
+    [Z,stats] = MatrixCompletion_MM(Mobs,lambdas(i),'Y0',Y0,'Display','off',...
+    'method','svt');
+    
+    if i >= 3
+        display(['Grid point ' num2str(i) ', rank=' num2str(stats.rank)]);
+    end
+end
+%profile viewer;
+toc;
+
+%% generate a random matrix with missing entries
+clear;
+
+s = RandStream('mt19937ar','Seed',2014);  % Reset random seed
+RandStream.setGlobalStream(s);
+
+p1 = 500;
+p2 = 500;
+r = 5;
+
+M = randn(p1,r) * randn(r,p2); %+ randn(p1,p2);
+display(rank(M));
+
+missingprop = 0.75;
+missingidx = rand(p1,p2)<missingprop;
+
+Mobs = M;
+Mobs(missingidx) = nan;
+
+%% find max lambda for nuclear norm regularization
+
+%[~,stats] = matrix_impute_Nesterov(Mobs,inf);
+[~,stats] = matrix_impute_MM(Mobs,inf);
+maxlambda = stats.maxlambda;
+disp(maxlambda);
+
+%%
+gridpts = 5;
+lambdas = exp(log(maxlambda)/gridpts*(gridpts:-1:1));
+
+display('full_svt');
+tic;
+%profile on;
+for i=1:gridpts
+    if (i==1)
+        Y0 = [];
+    else
+        Y0 = Z;
+    end
+    %[Z,stats] = matrix_impute_Nesterov(Mobs,lambdas(i),'Y0',Y0,'Display','off');
+    [Z,stats] = MatrixCompletion_MM(Mobs,lambdas(i),'Y0',Y0,'Display','off',...
+        'method','full');
+    
+    if i >= 3
+        display(['Grid point ' num2str(i) ', rank=' num2str(stats.rank)]);
+    end
+end
+%profile viewer;
+toc;
